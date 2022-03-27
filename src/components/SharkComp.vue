efeaa35335268170bd3af289e7e0d15e8f5a6bbb
HangDh <hangiedh@gmail.com>
2022-03-27 12:10:39 +0200

    Getting Vue and Flask to work together

---
+++ src/components/SharkComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button type="button" clas="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "SharkComp",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  methods: {
+    getResponse() {
+      const path = "http://localhost:5000/shark";
+
+      axios
+        .get(path)
+        .then((res) => {
+          console.log(res.data);
+          this.msg = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  mounted() {
+    this.getResponse();
+  },
+};
+</script>
